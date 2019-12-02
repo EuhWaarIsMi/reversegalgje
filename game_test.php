e8da2e4a2ecd24c609a8fae4bd69d3a416cf0f93
JohannesTvS <58428953+JohannesTvS@users.noreply.github.com>
2019-12-02 14:48:39 +0100

    Add files via upload

---
+++ game_test.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>test input</title>
+</head>
+
+<body>
+	<form>
+		<input type="button" value="ja"></input>
+		<input type="button" value="nee"></input>
+	</form>
+</body>
+</html>
---
+++ sessie_start.php
@@ -0,0 +1,13 @@
+<?php
+  $db = new PDO("sqlite:R:/root/phplite/reversegalgjeWOORDENVRAGEN.sqlite");
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$sessie = "test12";
+
+	$sql = "CREATE TABLE ".$sessie." AS SELECT id FROM woorden";
+	$db->query($sql);
+	$sql = "ALTER TABLE ".$sessie." ADD weg BOOL DEFAULT False";
+	$db->query($sql);
+	$sql = "ALTER TABLE ".$sessie." ADD laatstWeg BOOL DEFAULT False";
+	$db->query($sql);
+?>
