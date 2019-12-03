17e94156e6d1a2b1d3764593a24c0e623d32d583
sennederent <57815943+sennederent@users.noreply.github.com>
2019-12-03 17:42:23 +0100

    Add files via upload
    beetje aan gewerkt, niets bijzonders

--- game_test.php
+++ game_test.php
@@ -6,9 +6,26 @@
 </head>
 
 <body>
-	<form>
-		<input type="button" value="ja"></input>
-		<input type="button" value="nee"></input>
+	<?php
+		// hier komt vraag functie te staan
+		echo "<h1>ben je gay?</h1>";
+
+		$beurten = 10;
+		if(isset($_POST['antwoord'])) {
+			echo $_POST['antwoord'];
+			$beurten--;
+		}
+		
+		if(isset($_POST['zetTerug'])) {
+			echo $_POST['zetTerug'];
+		}
+		echo "<br>".$beurten;
+	?>
+
+	<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+		<input type="submit" name="antwoord" value="ja"></input>
+		<input type="submit" name="antwoord" value="nee"></input>
+		<input type="submit" name="zetTerug" value="ga een zet terug"></input>
 	</form>
 </body>
 </html>
