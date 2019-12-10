5c4852cd1280b8ae7b109db32f81a45d10f1435d
JohannesTvS <58428953+JohannesTvS@users.noreply.github.com>
2019-12-10 22:25:56 +0100

    Add files via upload

--- persoonlijkePagina.php
+++ persoonlijkePagina.php
@@ -1,37 +1,21 @@
 <?php
-	require "init.php";
+	require 'init.php';
+	session_start();
 ?>
+
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>test input</title>
+<title>Persoonlijke pagina</title>
 </head>
 
 <body>
-	<?php
-		// hier komt vraag functie te staan
-		echo "<h1>ben je gay?</h1>";
-
-		if(isset($_POST['antwoord'])) {
-			echo $_POST['antwoord'];
-			$beurten--;
-		}
-
-		if(isset($_POST['zetTerug'])) {
-			echo $_POST['zetTerug'];
-		}
-
-	?>
-
-	<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-		<input type="submit" name="antwoord" value="ja"></input>
-		<input type="submit" name="antwoord" value="nee"></input>
-		<input type="submit" name="zetTerug" value="ga een zet terug"></input>
-	</form>
-
-	<?php
-		KansBerekening($sessie, $kansSessie, $db);
-	?>
+Succes!
+<?php
+	echo $_SESSION['gebruiker'];
+	echo session_id();
+?>
+<a href="index.php">Home</a>
 </body>
 </html>
